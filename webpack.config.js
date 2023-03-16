9dbd2714299c8503856c283958ae72f74c695603
duvanpalomino <duvanpalomino@gmail.com>
2023-03-16 11:51:17 -0500

    loaders de fuentes en webpack

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,8 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js', 
+        filename: 'main.js',
+        assetModuleFilename: 'assets/images/[hash][ext][query]' 
     },
     resolve: {
         extensions: ['.js']
@@ -34,6 +35,20 @@ module.exports = {
             {
                 test: /\.png/,
                 type: 'asset/resource'
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                use: {
+                    loader: 'url-loader',
+                    options:{
+                        limit: 10000,
+                        mimetype: 'application/font-woff',
+                        name: '[name].[ext]',
+                        outputPath: './assets/fonts/',
+                        publicPath: './assets/fonts/',
+                        esModule: false,
+                    },
+                }
             }
         ]
     },
