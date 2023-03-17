f5819cb57d15c3b8899ec7e3f377043159c2298e
duvanpalomino <duvanpalomino@gmail.com>
2023-03-17 15:04:18 -0500

    webpack dev tools

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -16,6 +16,7 @@ module.exports = {
         assetModuleFilename: 'assets/images/[hash][ext][query]' 
     },
     mode: 'development',
+    devtool: 'source-map',
     // watch: true,
     resolve: {
         extensions: ['.js'],
